f7ba45f15160c3893d5433c3c4111d8f1f0d381b
Sadrawi <dr.sadrawi@gmail.com>
2025-06-25 15:05:26 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+import cv2
+from ultralytics import YOLO
+import tempfile
+
+# Load YOLOv8 segmentation model
+
+model = YOLO('best_arrhythmia.pt')
+
+# Streamlit UI
+st.title("Computer based Arrhythmia detection")
+
+uploaded_file = st.file_uploader("Upload an Image", 
+    type=['png', 'jpg', 'jpeg'])
+
+if uploaded_file:
+    image = Image.open(uploaded_file).convert("RGB")
+    st.image(image, 
+        caption="Uploaded Image", 
+        use_container_width=True)
+
+    if st.button("Run Segmentation"):
+        # Convert to numpy array
+        img_np = np.array(image)
+
+        # Run prediction
+        results = model.predict(img_np)[0]
+
+        # Visualize the mask overlay
+        seg_img = results.plot()  # returns a numpy array with the segmentation mask overlaid
+
+        # Show result
+        st.image(seg_img, 
+            caption="Detection Result", 
+            use_container_width=True)
+
