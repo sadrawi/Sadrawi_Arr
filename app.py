e8256c50fe7a8aa722405d44397360a603185e7f
Sadrawi <dr.sadrawi@gmail.com>
2025-10-29 16:23:56 +0700

    ok

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ if not os.path.exists(model_path):
 model = YOLO(model_path)
 
 # Streamlit UI
-st.title("Computer 00")
+st.title("Computer01")
 
 uploaded_file = st.file_uploader("Upload an Image", 
     type=['png', 'jpg', 'jpeg'])
