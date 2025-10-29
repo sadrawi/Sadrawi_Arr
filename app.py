4c41fdfacda96724913746f8f73507c4e984cdba
sadrawi <dr.sadrawi@gmail.com>
2025-10-29 16:19:36 +0700

    ok

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ if not os.path.exists(model_path):
 model = YOLO(model_path)
 
 # Streamlit UI
-st.title("Computer based Arrhythmia")
+st.title("Computer based")
 
 uploaded_file = st.file_uploader("Upload an Image", 
     type=['png', 'jpg', 'jpeg'])
